11f71eccf1e9177ddffc82f3d6c73a2c9f6d7b6c
iHaveNoKnowledge <hackdk12@gmail.com>
2023-03-29 12:06:04 +0700

    update custom component
    add underline cannot set inline style
    need to set in css class

--- PcBuilder00/src/components/CustomizationProto01.jsx
+++ PcBuilder00/src/components/CustomizationProto01.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import ImageIcon from "@mui/icons-material/Image";
-import {Box, List, ListItem, ListItemButton, ListItemText, ListItemAvatar, Avatar, CardMedia} from "@mui/material";
+import {Box, List, ListItem, ListItemButton, ListItemText, ListItemAvatar, Avatar, CardMedia, Button} from "@mui/material";
 import { changeCategory } from "../slices/categorySlice";
 import { removeProduct } from "../slices/cutomizeSliceNoApi";
+import './CustomizationProto01.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { margin, padding } from "@mui/system";
+
+
+
+
 
 const CustomizationProto01 = () => {
+  //useState
+  const [selected, setSelected] = useState(false);
  //useSelector
  const categories = useSelector((state) => state.noApiCustomize.partData);
   
@@ -25,74 +34,82 @@ const CustomizationProto01 = () => {
 
   return (
     <>
+      
       <List sx={{ width: "100%", bgcolor: "background.paper" }}>
         {categories.map((item, index) => {
           return (
             <React.Fragment key={index}>
+
               {item.id ? (
           
-                // เลือกสินค้าแล้ว  
-                // <div >    
-                  // <ListItem
-                  // sx={{
-                  //   ":not(:last-child)": {
-                  //     borderBottom: "0.2em solid rgba(0,0,0,0.20)"
-                  //   },
-                  //   marginLeft: "0.5em",
-                  //   // backgroundColor:"#FFFF"
-                  // }}
-                  // >
-                  //   <ListItemButton
-                  //     // sx={{borderLeft: "1em solid #42528A"}}
-                  //     onClick={(e) => {
-                  //       handleChange(item.category);
-                  //     }}
-                  //   >
-                  //     <ListItemAvatar>
-                  //       {/* <Avatar sx={{ borderRadius: "0" }}> */}
-                  //       {/* <ImageIcon /> */}
-                  //       <Box
-                  //         component="img"
-                  //         src={item.img}
-                  //         alt={item.title}
-                  //         sx={{ objectFit: "contain", width: 50 }}
-                  //       ></Box>
-                  //       {/* </Avatar> */}
-                  //     </ListItemAvatar>
-                  //     <ListItemText primary={item.title} secondary="" />
-                  //   </ListItemButton>
-                  // </ListItem>
-                  <ListItem sx={{border:"1px dashed green"}}>vv</ListItem>
-                // </div> 
+                //เลือกสินค้าแล้ว  
+                <div className="borderLine">
+                  <ListItem
+                    sx={{
+                      borderLeft: "0.8em solid #42528A",
+                      margin: "0px"
+                    }}
+                  >
+                    <ListItemButton
+                      disableRipple
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleChange(item.category);
+                      }}
+                    >
+                      <ListItemAvatar>
+                        <Box
+                          component="img"
+                          src={item.img}
+                          alt={item.title}
+                          sx={{ objectFit: "contain", width: 50 }}
+                        ></Box>
+                      </ListItemAvatar>
+                      <ListItemText primary={item.title} secondary="" />
+                      <Button
+                        disableRipple
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          console.log("innerBtnActivated");
+                        }}
+                      >
+                        test
+                      </Button>
+                    </ListItemButton>
+                  </ListItem>
+                </div> 
                 
               ) : (
 
                 // ยังไม่เลือกสินค้า
-                <ListItem
-                  sx={{
-                    ":not(:last-child)": {
-                      borderBottom: "0.2em solid rgba(0,0,0,0.20)"
-                    },
-                  }}
-                >
-                  <ListItemButton
-                    onClick={(e) => {
-                      handleChange(item.category);
+                <div className="borderLine">    
+                  <ListItem
+                    sx={{
+                      ":not(:last-child)": {},
+                      borderLeft: "0.8em solid #42528A",
+                      margin:"0px"
                     }}
                   >
-                    <ListItemAvatar>
-                      <Avatar sx={{ borderRadius: "0" }}>
-                        <ImageIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary={`${item.category}`} secondary="" />
-                  </ListItemButton>
-                </ListItem>
+                    <ListItemButton
+                      onClick={(e) => {
+                        handleChange(item.category);
+                      }}
+                    >
+                      <ListItemAvatar>
+                        <Avatar sx={{ borderRadius: "0" }}>
+                          <ImageIcon />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary={`${item.category}`} secondary="" />
+                    </ListItemButton>
+                  </ListItem>
+                  </div>
               )}
             </React.Fragment>
           );
         })}
       </List>
+
     </>
   );
 };
