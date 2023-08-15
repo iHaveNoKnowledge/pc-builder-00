809924a3f9adee04e8c8d5cb06ee462390590ad5
iHaveNoKnowledge <hackdk12@gmail.com>
2023-08-15 15:19:34 +0700

    Revert "Revert "Update BottomComponent""
    This reverts commit f8c2de9193addd55a99113578f2c973b450d38fd.

--- PcBuilder00/src/App.jsx
+++ PcBuilder00/src/App.jsx
@@ -5,25 +5,31 @@ import { Box, Container, Grid } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <Container disableGutters sx={{ height: "auto", maxWidth: "100%" }}>
-        <Grid container columns={{ xs: 4, sm: 12, md: 12 }} rowHeight={25}>
-          <Grid item xs={12} sm={12} md={12}>
-            <Box sx={{ px: 1 }}></Box>
-          </Grid>
-
-          <Grid item xs={4} sm={4} md={4}>
-            <CustomizationProto02 />
-          </Grid>
-
-          <Grid item xs={4} sm={8} md={8}>
-            <Box sx={{ pl: 2 }}>
-              <SelectionProto01 />
-            </Box>
-          </Grid>
+    <Container
+      disableGutters
+      sx={{
+        height: "auto",
+        "@media (min-width: 0px)": {
+          maxWidth: "1200px",
+        },
+      }}
+    >
+      <Grid container columns={{ xs: 4, md: 12 }} rowHeight={25}>
+        <Grid item xs={12} md={12}>
+          <Box sx={{ px: 1 }}></Box>
         </Grid>
-      </Container>
-    </div>
+
+        <Grid item xs={4} md={4}>
+          <CustomizationProto02 />
+        </Grid>
+
+        <Grid item xs={4} md={8}>
+          <Box sx={{ pl: 2 }}>
+            <SelectionProto01 />
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
--- PcBuilder00/src/components/BottomComponent.jsx
+++ PcBuilder00/src/components/BottomComponent.jsx
@@ -1,23 +1,36 @@
 import React, { lazy, Suspense } from "react";
-import Box from "@mui/material/Box";
+import { Box, Button } from "@mui/material";
 import "./BottomComponent.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./SetList";
 
 const componentsToLazyLoad = [
-  { path: () => import("./saveBuildForm"), label: "Save Build" },
+  { path: () => import("./saveBuildForm"), label: "Save Set" },
   { path: () => import("./SetList"), label: "Set List" },
-  { path: () => import("./ReportDocument"), label: "Report Document" },
+  { path: () => import("./ReportDocument"), label: "Report" },
   { path: () => import("./ReportCashier/AddSN"), label: "Add SN" },
 ];
 
 function Bottom() {
   return (
-    <Box className="mainCardBottom" sx={{ minHeight: "28px" }}>
-      {componentsToLazyLoad.map(({ path, label }) => (
-        <Suspense key={path.toString()} fallback={<></>}>
-          <Box>{React.createElement(lazy(path), { label })}</Box>
-        </Suspense>
-      ))}
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box className="mainCardBottom" sx={{ minHeight: "28px", position: "relative" }}>
+        {componentsToLazyLoad.map(({ path, label }) => (
+          <Suspense
+            key={path.toString()}
+            fallback={
+              <>
+                <Button variant="contained" color={label === "Add SN" ? "secondary" : "primary"}>
+                  {label}
+                </Button>
+              </>
+            }
+          >
+            <Box>{React.createElement(lazy(path), { label })}</Box>
+          </Suspense>
+        ))}
+      </Box>
+    </ThemeProvider>
   );
 }
 
